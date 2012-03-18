14af93a0f6b123060b24480adb06fd723e92afe5
twitches <jitterywombat@gmail.com>
2012-03-18 00:47:36 +0000

    Added setting persistence

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,19 +1,34 @@
 class MoviesController < ApplicationController
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
 
+  @@session_keys = [:ratings, :sort]
+
   def index
     @all_ratings = Movie.ratings
-    @filter_ratings = params[:ratings]
-    if not @filter_ratings
-      @filter_ratings = Hash[@all_ratings.map { |r| [r, 1]}]
+    # Grab the subset of our param and session keys that we care about.
+    param_keys = params.keys & @@session_keys
+    session_keys = session.keys & @@session_keys
+    if (param_keys | session_keys).size != param_keys.size
+      # There are session parameters that aren't in params.
+      params.update(Hash[session_keys.zip(session.values_at(session_keys))])
+      redirect_to
     end
-    sort = params[:sort] ? params[:sort].to_sym : nil
-    @movies = Movie.order(sort).find_all_by_rating(@filter_ratings.keys)
+    # Update our session parameters
+    session.update(params)
+
+    # Kind of need this to be set.
+    if session[:ratings]
+      @ratings = session[:ratings]
+    else
+      @ratings = Hash[@all_ratings.zip([1]*@all_ratings.size)]
+    end
+    sort = session[:sort] ? session[:sort].to_sym : nil
+    @movies = Movie.order(sort).
+      find_all_by_rating(@ratings.keys)
     @header_class = { sort => 'hilite'}
   end
 
